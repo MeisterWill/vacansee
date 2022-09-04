59ef62a823a3c291b458ae6de0365b665847716d
fives47 <humblias617@gmail.com>
2022-09-04 11:58:09 -0400

    API test attempt 1.5

--- test.js
+++ test.js
@@ -2,11 +2,13 @@ const request = new XMLHttpRequest();
 var APIKey = "ZC5iKcDhLMPPAuLuEbUpWkyinJXO3N99M2qpXhIe";
 const url = "http://api.rit.edu/v1/buildings/3,7A/meetings?date=2014-09-03&RITAuthorization=1973f96d944ea41d94950355261e61231236d3d76e";
 
-request.open('GET', url)
-request.responseType = 'json';
-request.send()
+function testThing(){
+    request.open('GET', url)
+    request.responseType = 'json';
+    request.send()
 
-request.onload = function(){
-    var responseObj = request.response;
-    alert(responseObj.message + "hello");
+    request.onload = function(){
+        var responseObj = request.response;
+        alert(responseObj.message + "hello");
+    }
 }
