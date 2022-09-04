8b3b81b2448472f453023ff690e922b8920b22d8
fives47 <humblias617@gmail.com>
2022-09-04 11:54:01 -0400

    API test attempt 1.1

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const request = new XMLHttpRequest();
 var APIKey = "ZC5iKcDhLMPPAuLuEbUpWkyinJXO3N99M2qpXhIe";
-const url = "http://api.rit.edu//v1/buildings/3,7A/meetings?date=2014-09-03&RITAuthorization=1973f96d944ea41d94950355261e61231236d3d76e";
+const url = "http://api.rit.edu/v1/buildings/3,7A/meetings?date=2014-09-03&RITAuthorization=1973f96d944ea41d94950355261e61231236d3d76e";
 
 request.open('GET', url)
 request.send()
