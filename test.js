b1fcda0240dc20ef1740850955789f3d5ab0b870
Will Meister <meisterwilliam5@gmail.com>
2022-09-04 12:13:50 -0400

    API test attempt 1.7

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
-const request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
 var APIKey = "ZC5iKcDhLMPPAuLuEbUpWkyinJXO3N99M2qpXhIe";
 const url = "http://api.rit.edu/v1/buildings/3,7A/meetings?date=2014-09-03&RITAuthorization=1973f96d944ea41d94950355261e61231236d3d76e";
 
 function testThing(){
-    request.open('GET', url)
+    alert("API Alert");
+    request.open('GET', url);
     request.responseType = 'json';
-    request.send()
+    request.send();
 
     request.onload = function(){
-        var responseObj = request.response();
+        var responseObj = request.response;
         alert(responseObj.message + "hello");
     }
 }
