1f8a4d2560cc5209e7af6b9cdb97777ba1b3de23
fives47 <humblias617@gmail.com>
2022-09-04 12:32:29 -0400

    Merging in progress

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 const request = new XMLHttpRequest();
 var APIKey = "ZC5iKcDhLMPPAuLuEbUpWkyinJXO3N99M2qpXhIe";
-const url = "http://api.rit.edu/v1/buildings/3,7A/meetings?date=2014-09-03&RITAuthorization=1973f96d944ea41d94950355261e61231236d3d76e";
+const url = "http://api.rit.edu/v1/RITAuthorization=1973f96d944ea41d94950355261e61231236d3d76e";
 
 function testThing(){
     request.open('GET', url)
     request.responseType = 'json';
     request.send()
 
-    request.onload = function(){
+    request.onreadystatechange = function(){
         var responseObj = request.response();
         alert(responseObj.message + "hello");
     }
